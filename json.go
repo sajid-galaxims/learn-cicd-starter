6227184b35c4cef84ffe7fcbc2fbdff30947a6ad
Sajid Manzoor <sajidm@Sajids-MacBook-Air.local>
2026-05-08 10:11:30 +0530

    Format Go code

--- json.go
+++ json.go
@@ -31,7 +31,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	}
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
-if err != nil {
-	// log the error
-}
+	if err != nil {
+		// log the error
+	}
 }
